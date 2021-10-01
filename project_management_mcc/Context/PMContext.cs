7f73ace126029025153c653ecc795b8061b22aa3
justhasbi <hasbishuhada123@gmail.com>
2021-10-01 08:30:23 +0700

    add dbContext and some configuration

---
+++ project_management_mcc/Context/PMContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace project_management_mcc.Context
+{
+    public class PMContext : DbContext
+    {
+        public PMContext(DbContextOptions<PMContext> options) : base(options)
+        {
+
+        }
+
+        // DbSet here
+    }
+}
