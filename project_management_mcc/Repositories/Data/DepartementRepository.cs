2017a90f63a784329a455cdcb254f85be63e2112
Pernandes <pernandessihombingg@gmail.com>
2021-10-02 18:47:27 +0700

    Class Data Repo

---
+++ project_management_mcc/Repositories/Data/DepartementRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace project_management_mcc.Repositories.Data
+{
+    public class DepartementRepository
+    {
+    }
+}
