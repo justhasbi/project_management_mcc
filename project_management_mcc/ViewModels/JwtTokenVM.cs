f31da87ea36321d927de9948b5ed655c0ab07198
Pernandes <pernandessihombingg@gmail.com>
2021-10-04 19:42:58 +0700

    login jwttoken

---
+++ project_management_mcc/ViewModels/JwtTokenVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace project_management_mcc.ViewModels
+{
+    public class JwtTokenVM
+    {
+        public string Messages { get; set; }
+        public string Token { get; set; }
+    }
+}
