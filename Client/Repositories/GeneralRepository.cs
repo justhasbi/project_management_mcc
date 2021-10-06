742626948efeff493d9dac4deacc42eef041aa9a
Pernandes <pernandessihombingg@gmail.com>
2021-10-06 16:15:51 +0700

    Add Client

---
+++ Client/Repositories/GeneralRepository.cs
@@ -0,0 +1,78 @@
+﻿using Client.Base.URL;
+using Client.Repositories.Interface;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Repositories
+{
+    public class GeneralRepository<TEntity, XKey> : IRepository<TEntity, XKey>
+        where TEntity : class
+    {
+        private readonly Address address;
+        private readonly string request;
+        //private readonly IHttpContextAccessor _contextAccessor;
+        private readonly HttpClient httpClient;
+
+        public GeneralRepository(Address address, string request)
+        {
+            this.address = address;
+            this.request = request;
+            //_contextAccessor = new HttpContextAccessor();
+            httpClient = new HttpClient
+            {
+                BaseAddress = new Uri(address.link)
+            };
+            //httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", _contextAccessor.HttpContext.Session.GetString("JWToken"));
+        }
+
+        public HttpStatusCode Delete(XKey key)
+        {
+            var result = httpClient.DeleteAsync(request + key).Result;
+            return result.StatusCode;
+        }
+
+        public async Task<List<TEntity>> Get()
+        {
+            List<TEntity> entities = new List<TEntity>();
+
+            using (var response = await httpClient.GetAsync(request))
+            {
+                string apiResponse = await response.Content.ReadAsStringAsync();
+                entities = JsonConvert.DeserializeObject<List<TEntity>>(apiResponse);
+            }
+            return entities;
+        }
+
+        public async Task<TEntity> Get(XKey key)
+        {
+            TEntity entity = null;
+
+            using (var response = await httpClient.GetAsync(request + key))
+            {
+                string apiResponse = await response.Content.ReadAsStringAsync();
+                entity = JsonConvert.DeserializeObject<TEntity>(apiResponse);
+            }
+            return entity;
+        }
+
+        public HttpStatusCode Put(XKey key, TEntity entity)
+        {
+            StringContent content = new StringContent(JsonConvert.SerializeObject(entity), Encoding.UTF8, "application/json");
+            var result = httpClient.PutAsync(request + key, content).Result;
+            return result.StatusCode;
+        }
+
+        public HttpStatusCode Post(TEntity entity)
+        {
+            StringContent content = new StringContent(JsonConvert.SerializeObject(entity), Encoding.UTF8, "application/json");
+            var result = httpClient.PostAsync(address.link + request, content).Result;
+            return result.StatusCode;
+        }
+    }
+}
---
+++ Client/Repositories/Interface/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Client.Repositories.Interface
+{
+    public interface IRepository<T, X>
+        where T : class
+    {
+        Task<List<T>> Get();
+        Task<T> Get(X key);
+        HttpStatusCode Post(T entity);
+        HttpStatusCode Put(X key, T entity);
+        HttpStatusCode Delete(X key);
+    }
+}
