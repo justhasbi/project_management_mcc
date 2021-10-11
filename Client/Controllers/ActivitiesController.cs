e32a5874179c277d8362f2b0dcaf151737f33dd1
justhasbi <hasbishuhada123@gmail.com>
2021-10-11 14:12:43 +0700

    add project details

---
+++ Client/Controllers/ActivitiesController.cs
@@ -0,0 +1,33 @@
+﻿using Client.Base.Controllers;
+using Client.Repositories.Data;
+using Microsoft.AspNetCore.Mvc;
+using project_management_mcc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Client.Controllers
+{
+    public class ActivitiesController : BaseController<Activity, ActivityRepository, int>
+    {
+        private readonly ActivityRepository repository;
+
+        public ActivitiesController(ActivityRepository repository) : base(repository)
+        {
+            this.repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<JsonResult> GetByProjectId(int id)
+        {
+            var result = await repository.GetByProjectId(id);
+            return Json(result);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
